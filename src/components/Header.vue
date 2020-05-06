4805ff4768c2ce8e706aa47a3727c5e02de86943
Thiago Maciel <thiagomrvieira@gmail.com>
2020-05-06 18:11:10 +0100

    Exibindo o Saldo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -25,6 +25,11 @@
                     </v-list-tile>
                 </v-list>
             </v-menu>
+            <v-layout align-center>
+                <span class="text-uppercase grey--text text--darken-2">
+                    Saldo: {{ funds }}
+                </span>
+            </v-layout>
         </v-toolbar-items>
 
     </v-toolbar>
@@ -33,7 +38,11 @@
 
 <script>
 export default {
-
+    computed: {
+        funds() {
+            return this.$store.getters.funds
+        }
+    }
 }
 </script>
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,31 @@
 <template>
-    <h1>Home</h1>
+    <div>
+        <h1 class="display-3 font-weight-light mb-4">Negocie e consulte suas ações</h1>
+        <v-sheet :elevation="6" class="pa-2 primary">
+            <v-icon class="white--text mr-3">info</v-icon>
+            <span class="headline white--text font-weight-light">
+                Você pode Salvar e Carregar os dados
+            </span>
+        </v-sheet>
+         <v-sheet :elevation="6" class="pa-2 success darken-1 mt-3">
+            <v-icon class="white--text mr-3">info</v-icon>
+            <span class="headline white--text font-weight-light">
+                Clique em 'Finalizar Dia' para começar um novo dia!
+            </span>
+        </v-sheet>
+        <v-divider class="my-4" />
+        <p class="display-1"><strong>Seu saldo: </strong>{{ funds }}</p>
+
+    </div>
 </template>
 
 <script>
 export default {
-
+    computed: {
+        funds() {
+            return this.$store.getters.funds
+        }
+    }
 }
 </script>
 
