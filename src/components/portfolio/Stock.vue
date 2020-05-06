db8328e281b0e35ccc5d39442e95287dfe439626
Thiago Maciel <thiagomrvieira@gmail.com>
2020-05-06 18:33:18 +0100

    Adicionando validações

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -13,10 +13,11 @@
         <v-card>
             <v-container fill-height>
                 <v-text-field label="Quantidade" type="number" 
+                    :error="insufficientQuantity || !Number.isInteger(quantity)"
                     v-model.number="quantity"/>
                 <v-btn class="blue darken-3 white--text"
-                    :disabled="quantity <= 0 || !Number.isInteger(quantity)"
-                    @click="sellStock" >Vender</v-btn>
+                    :disabled="insufficientQuantity || quantity <= 0 || !Number.isInteger(quantity)"
+                    @click="sellStock" >{{insufficientQuantity ? 'Insuficiente' : 'Vender'}}</v-btn>
             </v-container>
         </v-card>
     </v-flex>
@@ -31,6 +32,11 @@ export default {
         return {
             quantity: 0
         }
+    },
+    computed: {
+        insufficientQuantity(){
+            return this.quantity > this.stock.quantity
+        }        
     },
     methods: {
         ...mapActions({sellStockAction: 'sellStock'}),
