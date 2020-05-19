b6ac1d79fd818531153c780d1a2b7eaccd10d4c3
Thiago Maciel <thiagomrvieira@gmail.com>
2020-05-19 15:48:09 +0100

    Finalizando Dia - Alterando Preço

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,7 +13,7 @@
         <v-spacer></v-spacer>
 
         <v-toolbar-items>
-            <v-btn flat> Finalizar Dia</v-btn>
+            <v-btn flat @click="endDay"> Finalizar Dia</v-btn>
             <v-menu offset-y>
                 <v-btn flat slot="activator">Salvar & carregar</v-btn>
                 <v-list>
@@ -37,10 +37,19 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
     computed: {
         funds() {
             return this.$store.getters.funds
+        }
+
+    },
+    methods: {
+        ...mapActions(['randomizeStocks']),
+        endDay(){
+            this.randomizeStocks()
         }
     }
 }
